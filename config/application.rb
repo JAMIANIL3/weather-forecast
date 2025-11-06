79eb97e72ab12f264ecc8fe65c5344c2f7472a96
JAMIANIL3 <51171782+JAMIANIL3@users.noreply.github.com>
2025-11-06 08:37:51 +0000

    Fix Api::Config autoload path for Zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module WeatherForecast
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join("app/lib")
   end
 end
